39acc752b68c2425d1ece098461db0f53edca541
chihinchan <chihinchan@gmail.com>
2020-08-25 23:55:51 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,74 +1,61 @@
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
-    if (hero.isHittingTile(CollisionDirection.Right)) {
-        hero.setImage(rightSwordOutImg)
-    } else if (hero.isHittingTile(CollisionDirection.Left)) {
-        hero.setImage(leftSwordOutImg)
-    } else {
-    	
-    }
+	
 })
+function slideDown () {
+    hero.ay = 0
+    hero.vy = 15
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (hero.isHittingTile(CollisionDirection.Right) || hero.isHittingTile(CollisionDirection.Left) || hero.isHittingTile(CollisionDirection.Bottom)) {
         hero.vy = -300
     }
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    hero.setImage(leftFacingImg)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    hero.setImage(rightFacingImg)
+})
 let hero: Sprite = null
-let leftSwordOutImg: Image = null
-let rightSwordOutImg: Image = null
-rightSwordOutImg = img`
-    . . . . . . . f f . . . . . . . . . . . . . . . 
-    . . . . f f f f 2 f f . . . . . . . . . . . . . 
-    . . f f e e e e f 2 f f . . . . . . . . . . . . 
-    . f f e e e e e f 2 2 f f . . . . . . . . . . . 
-    . f e e e e f f e e e e f . . . . . . . . . . . 
-    . f f f f f e e 2 2 2 2 e f . . . . . . . . . . 
-    f f f e 2 2 2 f f f f e 2 f . . . . . . . . . . 
-    f f f f f f f f e e e f f f . . . . . . . . . . 
-    f e f e 4 4 e b f 4 4 e e f . . . . . . . . . . 
-    . f e e 4 d 4 b f d d e f . . . . . . . . . . . 
-    . . f e e e 4 d d d e e . c . . . . . . . . . . 
-    . . . f 2 2 2 2 e e d d e c c c c c c c . . . . 
-    . . . f 4 4 4 e 4 4 d d e c d d d d d . . . . . 
-    . . . f f f f f e e e e . c c c c c . . . . . . 
-    . . f f f f f f f f . . . c . . . . . . . . . . 
-    . . f f f . . f f . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
+let leftFacingImg: Image = null
+let rightFacingImg: Image = null
+let rightSwordOutImg = img`
+    . . . . . . . f f . . . . . . . 
+    . . . . f f f f 2 f f . . . . . 
+    . . f f e e e e f 2 f f . . . . 
+    . f f e e e e e f 2 2 f f . . . 
+    . f e e e e f f e e e e f . . . 
+    . f f f f f e e 2 2 2 2 e f . . 
+    f f f e 2 2 2 f f f f e 2 f . . 
+    f f f f f f f f e e e f f f . . 
+    f e f e 4 4 e b f 4 4 e e f . . 
+    . f e e 4 d 4 b f d d e f . . . 
+    . . f e e e 4 d d d e e . c . . 
+    . . . f 2 2 2 2 e e d d e c c c 
+    . . . f 4 4 4 e 4 4 d d e c d d 
+    . . . f f f f f e e e e . c c c 
+    . . f f f f f f f f . . . c . . 
+    . . f f f . . f f . . . . . . . 
     `
-leftSwordOutImg = img`
-    . . . . . . . f f . . . . . . . . . . . . . . . 
-    . . . . f f f f 2 f f . . . . . . . . . . . . . 
-    . . f f e e e e f 2 f f . . . . . . . . . . . . 
-    . f f e e e e e f 2 2 f f . . . . . . . . . . . 
-    . f e e e e f f e e e e f . . . . . . . . . . . 
-    . f f f f f e e 2 2 2 2 e f . . . . . . . . . . 
-    f f f e 2 2 2 f f f f e 2 f . . . . . . . . . . 
-    f f f f f f f f e e e f f f . . . . . . . . . . 
-    f e f e 4 4 e b f 4 4 e e f . . . . . . . . . . 
-    . f e e 4 d 4 b f d d e f . . . . . . . . . . . 
-    . . f e e e 4 d d d e e . c . . . . . . . . . . 
-    . . . f 2 2 2 2 e e d d e c c c c c c c . . . . 
-    . . . f 4 4 4 e 4 4 d d e c d d d d d . . . . . 
-    . . . f f f f f e e e e . c c c c c . . . . . . 
-    . . f f f f f f f f . . . c . . . . . . . . . . 
-    . . f f f . . f f . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . . . . . . . . . 
+let leftSwordOutImg = img`
+    . . . . . . . f f . . . . . . . 
+    . . . . . f f 2 f f f f . . . . 
+    . . . . f f 2 f e e e e f f . . 
+    . . . f f 2 2 f e e e e e f f . 
+    . . . f e e e e f f e e e e f . 
+    . . f e 2 2 2 2 e e f f f f f . 
+    . . f 2 e f f f f 2 2 2 e f f f 
+    . . f f f e e e f f f f f f f f 
+    . . f e e 4 4 f b e 4 4 e f e f 
+    . . . f e d d f b 4 d 4 e e f . 
+    . . c . e e d d d 4 e e e f . . 
+    c c c e d d e e 2 2 2 2 f . . . 
+    d d c e d d 4 4 e 4 4 4 f . . . 
+    c c c . e e e e f f f f f . . . 
+    . . c . . . f f f f f f f f . . 
+    . . . . . . . f f . . f f f . . 
     `
-leftSwordOutImg.flipX()
-hero = sprites.create(img`
+rightFacingImg = img`
     . . . . . . . . . . . . . . . . 
     . . . . . f f f f f f . . . . . 
     . . . f f e e e e f 2 f . . . . 
@@ -85,7 +72,26 @@ hero = sprites.create(img`
     . . . f f f e e f 5 5 f f . . . 
     . . . f f f f f f f f f f . . . 
     . . . . f f . . . f f f . . . . 
-    `, SpriteKind.Player)
+    `
+leftFacingImg = img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . f 2 f e e e e f f . . . 
+    . . . f 2 2 2 f e e e e f f . . 
+    . . . f e e e e f f e e e f . . 
+    . . f e 2 2 2 2 e e f f f f . . 
+    . . f 2 e f f f f 2 2 2 e f . . 
+    . . f f f e e e f f f f f f f . 
+    . . f e e 4 4 f b e 4 4 e f f . 
+    . . . f e d d f 1 4 d 4 e e f . 
+    . . . . f d d d e e e e e f . . 
+    . . . . f e 4 e d d 4 f . . . . 
+    . . . . f 2 2 e d d e f . . . . 
+    . . . f f 5 5 f e e f f f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . f f f . . . f f . . . . 
+    `
+hero = sprites.create(rightFacingImg, SpriteKind.Player)
 controller.moveSprite(hero, 100, 0)
 tiles.setTilemap(tiles.createTilemap(hex`0a0020000303030303030303030303030303030303030303030303030303030303020303030301010101010103030303010303030303030303030103030303030301030303030303030303010303030303010303030103030303030103030301030303030301030303030303030303010303030303030303030101030303030303030303010303030303030303030303030303010303030303030303030103030303030303030301030303010303030303010303030103030303030103030303030303030301030303030301030303010303030303010303030303030303030103030303030303030301030301030303030303010303010103030303030103030303030103030301030103030301030303010301030303010103030303030303030303030303030303030303030303030303030303030303030301010101010101010101`, img`
     . . . . . . . . . . 
@@ -124,3 +130,19 @@ tiles.setTilemap(tiles.createTilemap(hex`0a0020000303030303030303030303030303030
 tiles.placeOnTile(hero, tiles.getTileLocation(5, 30))
 scene.cameraFollowSprite(hero)
 hero.ay = 350
+game.onUpdateInterval(100, function () {
+    if (hero.isHittingTile(CollisionDirection.Right)) {
+        hero.setImage(rightSwordOutImg)
+        slideDown()
+    } else if (hero.isHittingTile(CollisionDirection.Left)) {
+        hero.setImage(leftSwordOutImg)
+        slideDown()
+    } else {
+        hero.ay = 350
+        if (hero.image == leftSwordOutImg) {
+            hero.setImage(leftFacingImg)
+        } else if (hero.image == rightSwordOutImg) {
+            hero.setImage(rightFacingImg)
+        }
+    }
+})
